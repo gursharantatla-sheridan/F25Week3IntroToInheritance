000d39cdbde2545f9eca5834deb83ae52cc4c04a
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-09-17 09:23:49 -0400

    Add project files.

---
+++ F25Week3IntroToInheritance/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace F25Week3IntroToInheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
