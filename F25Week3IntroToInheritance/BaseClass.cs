e4074c5ad680d05992f9f40987e45587c8c264c5
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-09-17 09:40:57 -0400

    constructor calls

---
+++ F25Week3IntroToInheritance/BaseClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F25Week3IntroToInheritance
+{
+    public class BaseClass
+    {
+        private int myPrivateVar;
+        protected int myProtectedVar;
+        public int myPublicVar;
+
+        public BaseClass()
+        {
+            Console.WriteLine("Base class constructor called");
+        }
+    }
+
+    public class DerivedClass : BaseClass
+    {
+        public void myMethod()
+        {
+            //myPrivateVar = 101;
+            myProtectedVar = 102;
+            myPublicVar = 103;
+        }
+    }
+}
